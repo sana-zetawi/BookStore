d20215bff716fe1417059cec27081c36fce7f74b
sanazetawi-rawanhassoun <78093292+sana-zetawi@users.noreply.github.com>
2022-04-07 09:59:11 +0300

    update

--- order.py
+++ order.py
@@ -7,8 +7,14 @@ app = Flask(__name__)
 
 @app.route("/purchase/<item_number>",methods=['PUT'])
 def purchase(item_number):
-    result=requests.get("http://192.168.1.18:4000/decreas/"+str(item_number),data={'amount':1})
-    return (result.content)
+    result=requests.put("http://127.0.0.1:4000/decrease/"+str(item_number),data={'amount': 1})
+    s=result.json()
+    masage=str(s.get('Successful'))
 
+    if(masage =='Decrease The Number Of Items In Stock'):
+         result=requests.get("http://127.0.0.1:4000/info/"+str(item_number)) 
+         return (result.content)
+    
+    return("no book in stock")
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0', port=7000)
